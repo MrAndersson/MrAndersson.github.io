913c45c1f9ff2fdb3e61905477fa4913e73f8c76
MrAndersson <Mtktwoo@gmail.com>
2017-09-01 13:53:32 +0200

    Frolb

--- JS/timeAndDate.js
+++ JS/timeAndDate.js
@@ -1,3 +1,9 @@
+"use strict";
+
+/*var toggleButton = $("#toggleButton");
+toggleButton.on("click", togglePI() {});*/
+
+
 function warnMe()
 {
     alert("ZugZug Booga")
@@ -5,12 +11,14 @@ function warnMe()
 
 function genRandomColor()
 {
-    return (`rgb( ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`);
+    var rgb = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+    return rgb;
 }
 
 function changeOverlayColor()
 {
-    document.getElementById('overlay').style.background = this.genRandomColor();
+    document.getElementById('overlay').style.background = genRandomColor();
+    //$("#overlay").style.background = genRandomColor();
 
     var t = setTimeout(changeOverlayColor, Math.round(Math.random() * 500));
 }
@@ -18,6 +26,7 @@ function changeOverlayColor()
 function changeBackgroundColor()
 {
     //document.body.style.background = this.genRandomColor();
+    //$("#body").style.background = this.genRandomColor();
 
     var t = setTimeout(changeBackgroundColor, Math.round(Math.random() * 1000));
 }
@@ -25,29 +34,40 @@ function changeBackgroundColor()
 function randomNumber()
 {
     var a = Math.random();
-    document.getElementById('randNum').innerHTML = a;
+    //document.getElementById('randNum').innerHTML = a;
+    $("#randNum").text(a);
     var t = setTimeout(randomNumber, Math.round(a * 1000));
 }
 
-function rotateImage()
+function togglePI()
 {
-    document.getElementById('myImg').innerHTML;
-};
+    console.log("Toggle Pi!");
 
-function showPi()
-{
-    document.getElementById('pi').innerHTML = Math.PI;
+    var piToggle = false;
+    if(!piToggle)
+    {
+        $("#pi").text(Math.PI);
+        //document.getElementById('pi').innerHTML = Math.PI;
+    }
+    else
+    {
+        $("#pi").text("");
+        //document.getElementById('pi').innerHTML = Math.PI;
+    }
+
 }
 
 function currentTime()
 {
     var today = new Date();
-    h = checkTime(today.getHours());
-    m = checkTime(today.getMinutes());
-    s = checkTime(today.getSeconds());
+    var h = checkTime(today.getHours());
+    var m = checkTime(today.getMinutes());
+    var s = checkTime(today.getSeconds());
 
     // Fetch an portion of the HTML script by ID
-    document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+    //document.getElementById('clock').innerHTML = h + ":" + m + ":" + s;
+    var clock = $("#clock");
+    clock.text(`${h}:${m}:${s}`);
     // The setTimeout() method calls a function or evaluates an expression after a specified number of milliseconds.
     var t = setTimeout(currentTime, 500);
 };
@@ -56,7 +76,7 @@ function checkTime(i)
 {
     if(i < 10)
     {
-        i = "0" + i;
+        i = `0${i}`
     }
     return i;
 };
