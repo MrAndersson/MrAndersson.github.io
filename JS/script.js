458ed7fe33dd85aa3bc444d212f551197623789d
MrAndersson <Mtktwoo@gmail.com>
2017-09-15 14:06:03 +0200

    blarf

--- JS/script.js
+++ JS/script.js
@@ -1,128 +1,105 @@
 "use strict";
 
 /// Makes sure the DOM is ready before start to load functions, jQuery
-$(document).ready(function()
-{
-    var toggleButton = $("#toggleButton");
-    toggleButton.on("click", function() 
-    {
-        var pi = $("#pi");
-
-        if(toggleButton.text() === "Show PI!")
-        {
-            toggleButton.text("Hide PI!");
-            pi.text(Math.PI);
-        }
-        else
-        {
-            toggleButton.text("Show PI!");
-            pi.text("");
-        }
-    });
-
+$(document).ready(function () {
     var results = [];
-    
+
     /// New cool object
-    function person(ID, occupation, human)
-    {
+    function person(ID, occupation, human) {
         this.id = ID;
         this.occupation = occupation;
         this.human = human;
     }
-    
+
     var blorf1 = new person("66 666 66", "Demon", false);
     var blorf2 = new person("22 222 22", "Paladin", true);
     var blorf3 = new person("00 111 00", "Druid", false);
-    
+
     results.push(blorf1); results.push(blorf2); results.push(blorf3);
     var resultList = $("#randomList");
     resultList.empty();
-    
-    $.each(results, function(i, item)
-    {
+
+    $.each(results, function (i, item) {
         var textResult = $(`<div class='result'>
                             <div class='title'>ID: ${item.id} </div>
                             <div>Occupation: ${item.occupation} </div>
                             <div>Human: ${item.human} </div>
                             </div>`);
 
-        textResult.hover(function()
-        {
-            $(this).css("background-color", "lightblue");
-        }, 
-        function()
-        {
-            $(this).css("background-color", "transparent");
-        });
+        textResult.hover(function () {
+            $(this).css("background-color", "pink");
+        },
+            function () {
+                $(this).css("background-color", "transparent");
+            });
 
-        $(textResult).css({"border-color": "#D3D3D3", "border-width":"1px", "border-style":"solid"});
+        $(textResult).css({ "border-color": "#D3D3D3", "border-width": "1px", "border-style": "solid" });
 
         resultList.append(textResult);
     });
 
     var callbackButton = $("#callbackButton");
-    callbackButton.on("click", function()
-    {print($("#callbackText"), "Booga Booga")});
+    callbackButton.on("click", function ()
+    { print($("#callbackText"), "Booga Booga") });
 });
 
-function print(HTMLPageID, text)
-{
+function print(HTMLPageID, text) {
     $(HTMLPageID).text(`${text}, this is a callback function`);
 }
 
-(function()
-{
+(function () {
     var app = angular.module('myApp', []);
 
     /////////////////
     //  Functions  //
     /////////////////
 
-    function MainCtrl($scope)
-    {
+    function MainCtrl($scope) {
         $scope.helloworld = "Hello World!";
     }
 
-    function RandomNumberCtrl()
-    {
+    function TogglePI($scope) {
+        $scope.toggle = function () {
+            if ($scope.show == "")
+                $scope.show = Math.PI;
+            else
+                $scope.show = "";
+        };
+    }
+
+    function RandomNumberCtrl() {
         var a = Math.random();
         return a;
     }
 
-    function TimeCtrl()
-    {
+    function TimeCtrl() {
         var today = new Date();
         var h = checkTime(today.getHours());
         var m = checkTime(today.getMinutes());
         var s = checkTime(today.getSeconds());
-    
+
         // Fetch an portion of the HTML script by ID
         return (`${h}:${m}:${s}`);
     };
 
-    function checkTime(i)
-    {
-        if(i < 10)
-        {
+    function checkTime(i) {
+        if (i < 10) {
             i = `0${i}`
         }
         return i;
     };
 
-    function RandomColorCtrl()
-    {
+    function RandomColorCtrl() {
         var rgb = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
         return rgb;
     }
-    
-    function changeOverlayColor()
-    {
+
+    function changeOverlayColor() {
         document.getElementById('overlay').style.background = RandomColorCtrl();
     }
 
-    function RandomNumerForTime(multiplyer = 1)
-    {
-       return (Math.round(Math.random() * 500));
+    function RandomNumerForTime(multiplyer = 1) {
+        return (Math.round(Math.random() * 500));
     };
 
     /////////////////
@@ -131,19 +108,17 @@ function print(HTMLPageID, text)
 
     //app.controller(Define func, Which func to use);
     app.controller('MainCtrl', ["$scope", MainCtrl]);
-    app.controller('RandomNumberCtrl', function($scope, $interval)
-    {
+    app.controller('RandomNumberCtrl', function ($scope, $interval) {
         $scope.randNum = RandomNumberCtrl();
-        $interval(function(){$scope.randNum = RandomNumberCtrl()}, RandomNumerForTime());
+        $interval(function () { $scope.randNum = RandomNumberCtrl() }, RandomNumerForTime());
     });
-    app.controller('TimeCtrl', function($scope, $interval)
-    {
+    app.controller('TimeCtrl', function ($scope, $interval) {
         $scope.clock = TimeCtrl()
-        $interval(function(){$scope.clock = TimeCtrl()}, 500);
+        $interval(function () { $scope.clock = TimeCtrl() }, 500);
     });
-    app.controller('RandomColorCtrl', function($scope, $interval)
-    {
+    app.controller('RandomColorCtrl', function ($scope, $interval) {
         changeOverlayColor();
-        $interval(function(){changeOverlayColor()}, RandomNumerForTime(500));
+        $interval(function () { changeOverlayColor() }, RandomNumerForTime(500));
     });
+    app.controller('TogglePI', ["$scope", TogglePI]);
 }());
